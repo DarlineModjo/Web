fec765be140cdd4e938d841c306c57edf23fb4e1
wanas2 <snawilfried@gmail.com>
2022-05-27 17:44:28 +0100

    Website redesign

---
+++ src/components/SideBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+            <div class="nav">
+                <router-link class="nav-link" to="/">
+                    <div class="sb-nav-link-icon">
+                        <i class="fas fa-home"></i>
+                    </div>
+                    Accueil
+                </router-link>
+                <router-link class="nav-link" to="/products">
+                    <div class="sb-nav-link-icon">
+                        <i class="fas fa-product-hunt"></i>
+                    </div>
+                    Produits
+                </router-link>
+                <router-link class="nav-link" to="/services">
+                    <div class="sb-nav-link-icon">
+                        <i class="fa-solid fa-hand-holding"></i>
+                    </div>
+                    Services
+                </router-link>
+
+                <div class="sb-sidenav-menu-heading">Authentification</div>
+                <div class="d-flex align-items-center">
+                    <router-link class="nav-link" to="/login">
+                        Connexion
+                    </router-link>
+                    <span>|</span>
+                    <router-link class="nav-link" to="/register">
+                        Inscription
+                    </router-link>
+                    
+                </div>
+                
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'SideBar'
+    }
+</script>
